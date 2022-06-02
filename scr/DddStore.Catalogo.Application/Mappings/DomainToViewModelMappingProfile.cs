86a58c9dc4f84f1a3e4649e8c9a100cde457f38c
Leonardo Gasparini Romão <lleonardogr@gmail.com>
2022-06-02 17:07:26 -0300

    feat: Camada de Application

---
+++ scr/DddStore.Catalogo.Application/Mappings/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DddStore.Catalogo.Application.ViewModels;
+using DddStore.Catalogo.Domain;
+
+namespace DddStore.Catalogo.Application.Mappings
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(d => d.Largura, o => o.MapFrom(s => s.Dimensoes.Largura))
+                .ForMember(d => d.Altura, o => o.MapFrom(s => s.Dimensoes.Altura))
+                .ForMember(d => d.Profundidade, o => o.MapFrom(s => s.Dimensoes.Profundidade));
+            CreateMap<Categoria, CategoriaViewModel>();
+        }
+    }
+}
---
+++ scr/DddStore.Catalogo.Application/Mappings/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DddStore.Catalogo.Application.ViewModels;
+using DddStore.Catalogo.Domain;
+
+
+namespace DddStore.Catalogo.Application.Mappings
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<ProdutoViewModel, Produto>()
+                .ConstructUsing(p => new Produto(p.Nome, p.Descricao, p.Ativo,
+                p.Valor, p.CategoriaId, p.DataCadastro,
+                p.Imagem, new Dimensoes(p.Altura, p.Largura, p.Profundidade)));
+
+            CreateMap<CategoriaViewModel, Categoria>()
+                .ConvertUsing(c => new Categoria(c.Nome, c.Codigo));
+        }
+    }
+}
